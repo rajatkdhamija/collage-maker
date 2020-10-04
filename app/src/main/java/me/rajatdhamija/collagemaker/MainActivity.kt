831c4aabb29c314ba797087407d820dfcb934f57
Rajat <rajatkdhamija@gmail.com>
2020-10-04 22:19:14 +0530

    added basic layouts for layout orientations

--- app/src/main/java/me/rajatdhamija/collagemaker/MainActivity.kt
+++ app/src/main/java/me/rajatdhamija/collagemaker/MainActivity.kt
@@ -1,11 +1,47 @@
 package me.rajatdhamija.collagemaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inflater: LayoutInflater =
+            this.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val childLayout: View = inflater.inflate(
+            R.layout.format_four_square,
+            findViewById(R.id.four_square)
+        )
+//        llMain.addView(childLayout)
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap? {
+        //Define a bitmap with the same size as the view
+        val returnedBitmap: Bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+        //Get the view's background
+        val bgDrawable: Drawable = view.background
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        }
+        // draw the view on the canvas
+        view.draw(canvas)
+        //return the bitmap
+        return returnedBitmap
     }
 }
